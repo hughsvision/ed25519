d172c1386c3a4350e08f8fd285314353f86bcdf6
hupeng <hupengbuaa@gmail.com>
2018-04-18 16:33:47 +0800

    c_src ref to https://github.com/orlp/ed25519 with few change build and test keypair()  nif

---
+++ c_src/ed25519_nifs.c
@@ -0,0 +1,81 @@
+#include "ed25519.h"
+#include "erl_nif.h"
+#include "erl_nif_compat.h"
+// prototypes
+ERL_NIF_TERM ed25519_keypair(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+//ERL_NIF_TERM ed25519_update(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+//ERL_NIF_TERM ed25519_final(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+//ERL_NIF_TERM ed25519_hash(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+ERL_NIF_TERM make_error_tuple(ErlNifEnv *env, char *error);
+
+// lifecycle
+int load(ErlNifEnv* env, void ** priv_data, ERL_NIF_TERM load_info);
+int reload(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info);
+int upgrade(ErlNifEnv* env, void** priv, void** old_priv, ERL_NIF_TERM load_info);
+void unload(ErlNifEnv* env, void* priv);
+
+static ErlNifFunc nif_funcs[] = 
+{
+	{"keypair", 0, ed25519_keypair},
+};
+
+ERL_NIF_INIT(ed25519, nif_funcs, load, NULL, NULL, NULL)
+
+int load(ErlNifEnv* env, void ** priv_data, ERL_NIF_TERM load_info)
+{
+	return 0;
+}
+
+int reload(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
+{
+	return 0;
+}
+
+int upgrade(ErlNifEnv* env, void** priv, void** old_priv, ERL_NIF_TERM load_info)
+{
+	return 0;
+}
+
+void unload(ErlNifEnv* env, void* priv)
+{
+	return;
+}
+
+ERL_NIF_TERM ed25519_keypair(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+	ErlNifBinary secret;
+	ErlNifBinary public;
+	ErlNifBinary seed;
+	// handle badarg
+		
+	if (!enif_alloc_binary(64, &secret)){
+		return make_error_tuple(env, "alloc_secret_failed");
+	}
+
+	if (!enif_alloc_binary(32, &public)){
+		return make_error_tuple(env, "alloc_public_failed");
+	}
+
+	if (!enif_alloc_binary(32, &seed)){
+		return make_error_tuple(env, "alloc_seed_failed");
+	}
+
+	ed25519_create_seed(seed.data);
+	int result = ed25519_create_keypair(public.data, secret.data, seed.data);
+	if (result!=0){
+		return make_error_tuple(env, "ed25519_create_keypair_failed");
+	}else{
+		return enif_make_tuple3(env, 
+				enif_make_atom(env, "ok"), 
+				enif_make_binary(env, &secret),
+				enif_make_binary(env, &public));
+	}
+	
+}
+
+ERL_NIF_TERM make_error_tuple(ErlNifEnv *env, char *error)
+{
+    return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_atom(env, error));
+}
+
+
---
+++ src/ed25519.erl
@@ -0,0 +1,38 @@
+-module(ed25519).
+
+-export([keypair/0,
+	 public_key/1]).
+
+-type signature() :: binary().
+-type secret() :: binary().
+-type public() :: binary().
+-on_load(init/0).
+
+-define(APPNAME, ed25519).
+-define(LIBNAME, ed25519).
+
+-spec keypair() -> {ok, secret(), public()} | {error, atom()}.
+keypair() ->
+	"NIF library not loaded".
+
+-spec public_key(secret()) -> {ok, public()} | {error, atom()}.
+public_key(SecretKey) -> 
+	ed25519:public_key(SecretKey).
+
+init() ->
+	SoName = case code:priv_dir(?APPNAME) of
+			 {error, bad_name} ->
+				 case filelib:is_dir(filename:join(["..", priv])) of
+					 true ->
+						 filename:join(["..", priv, ?LIBNAME]);
+					 _ ->
+						 filename:join([priv, ?LIBNAME])
+				 end;
+			 Dir ->
+				 filename:join(Dir, ?LIBNAME)
+		 end,
+	erlang:load_nif(SoName, 0).
+
+
+
+
